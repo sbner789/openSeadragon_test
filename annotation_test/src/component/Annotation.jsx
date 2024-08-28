79a6ec956a290d3a518af7ac4643cd0df6f7f076
rickchoi <sbner182@gmail.com>
2024-08-28 17:48:42 +0900

    edit

--- annotation_test/src/component/Annotation.jsx
+++ annotation_test/src/component/Annotation.jsx
@@ -3,36 +3,16 @@ import OpenSeadragon from "../viewer/openseadragon/openseadragon";
 import TestImage from "../test_image/test_image1.jpg";
 
 const Annotation = () => {
-    // const [viewer, setViewer] = useState(null);
-    // const InitOpenSeadragon = () => {
-    //     viewer && viewer.destroy();
-    //     setViewer(
-    //         OpenSeadragon({
-    //             id: "openSeaDragon",
-    //             animationTime: 0.5,
-    //             blendTime: 0.1,
-    //             constrainDuringPan: true,
-    //             maxZoomPixelRatio: 2,
-    //             minZoomLevel: 1,
-    //             visibilityRatio: 1,
-    //             zoomPerScroll: 2,
-    //             tileSources: {
-    //                 type: "image",
-    //                 levels: [
-    //                     {
-    //                         url: TestImage,
-    //                         width: 3840,
-    //                         height: 2160,
-    //                     }
-    //                 ]
-    //             },
-    //         })
-    //     )
-    // }
 
     useEffect(() => {
         const viewer = OpenSeadragon({
             id: "openSeaDragon",
+            animationTime: 0.5,
+            blendTime: 0.1,
+            constrainDuringPan: true,
+            maxZoomPixelRatio: 2,
+            minZoomLevel: 1,
+            zoomPerScroll: 2,
             tileSources: {
                 type: 'image',
                 url: TestImage
